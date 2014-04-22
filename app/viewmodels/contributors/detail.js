3108185c3c9511d8fcae460b61f4ae8aa062a716
Kennethtruyers <truyers.kenneth@gmail.com>
2014-04-22 15:26:34 +0200

    Added computed observable

--- app/viewmodels/contributors/detail.js
+++ app/viewmodels/contributors/detail.js
@@ -1,9 +1,13 @@
-﻿define(['services/http'],
-    function (repo) {
+﻿define(['services/http', 'plugins/observable'],
+    function (repo, observable) {
 
         var self = {};
 
         self.contributor = {};
+
+        observable.defineProperty(self, 'FullName', function() {
+            return self.contributor.FirstName + ' ' + self.contributor.LastName;
+        });
 
         self.activate = function (id) {
             return repo.get('contributors/' + id)
