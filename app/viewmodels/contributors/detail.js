41e8d236192141b560b1561e64524d2b011f24bb
Kennethtruyers <truyers.kenneth@gmail.com>
2014-04-22 14:55:36 +0200

    Included detail page

---
+++ app/viewmodels/contributors/detail.js
@@ -0,0 +1,17 @@
+﻿define(['services/http'],
+    function (repo) {
+
+        var self = {};
+
+        self.contributor = {};
+
+        self.activate = function (id) {
+            return repo.get('contributors/' + id)
+                       .done(function (result) {
+                           self.contributor = result;
+                       });
+        };
+
+        return self;
+
+    });
